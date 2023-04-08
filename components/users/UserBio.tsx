853f2e18a2f9bfd58678476f09bf47706da64e23
gauravsharma29 <writeonsharma@gmail.com>
2023-04-09 01:29:29 +0800

    Post and Likes modules completed

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -4,7 +4,7 @@ import { format } from "date-fns";
 
 import useCurrentUser from "../../hooks/useCurrentUser";
 import useUser from "../../hooks/useUser";
-// import useFollow from "../../hooks/useFollow";
+import useFollow from "../../hooks/useFollow";
 
 import Button from "../Button";
 import useEditModal from "../../hooks/useEditModal";
@@ -19,7 +19,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
 
   const editModal = useEditModal();
 
-  //   const { isFollowing, toggleFollow } = useFollow(userId);
+  const { isFollowing, toggleFollow } = useFollow(userId);
 
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) {
@@ -36,8 +36,7 @@ const UserBio: React.FC<UserBioProps> = ({ userId }) => {
         {currentUser?.id === userId ? (
           <Button secondary label="Edit" onClick={editModal.onOpen} />
         ) : (
-          <Button onClick={() => {}} label={true ? "Unfollow" : "Follow"} secondary={false} outline={true} />
-          //   <Button onClick={toggleFollow} label={isFollowing ? "Unfollow" : "Follow"} secondary={!isFollowing} outline={isFollowing} />
+          <Button onClick={toggleFollow} label={isFollowing ? "Unfollow" : "Follow"} secondary={!isFollowing} outline={isFollowing} />
         )}
       </div>
       <div className="mt-8 px-4">
